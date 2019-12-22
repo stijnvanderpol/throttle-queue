a8ad4f776dfaf837ab215d739d05b9cf8529ad7b
Stijn van der Pol <s.o.vanderpol@gmail.com>
2019-12-22 18:00:47 +0100

    add a test that verifies the first call is invoked right away by default whilst the other calls are called after a delay

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,28 @@
+import { stub } from 'sinon';
 import { queue } from './index';
 
-describe('temp', () => {
-    it('returns true', () => {
-        expect(queue).toBeTruthy();
-    })
+describe('queue', () => {
+    it('invokes the first call right away and delays subsequent calls', (finish) => {
+        const callbackStub = stub();
+        const queuedCallbackStub = queue(callbackStub, 200);
+
+        expect(callbackStub.callCount).toStrictEqual(0);
+
+        queuedCallbackStub();
+        queuedCallbackStub();
+        queuedCallbackStub();
+    
+        setTimeout(() => {
+            expect(callbackStub.callCount).toStrictEqual(1);
+        }, 100);
+        
+        setTimeout(() => {
+            expect(callbackStub.callCount).toStrictEqual(2);
+        }, 300);
+
+        setTimeout(() => {
+            expect(callbackStub.callCount).toStrictEqual(3);
+            finish();
+        }, 500);
+    });
 })
