62afb115cfc6e0be639d0ac17fb591d291b87aa7
Stijn van der Pol <s.o.vanderpol@gmail.com>
2019-12-23 16:03:07 +0100

    add a function description to queue

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,13 @@ const defaultOptions: Options = {
     skipInitialDelay: true
 };
 
+/**
+ * Returns a queued function that when called will store the calls in a queue and execute them one by one after the provided delay.
+ * The first call will be executed in the next frame unless configured otherwise. 
+ * @param callback The function to transform into a queued function.
+ * @param delay    The time in milliseconds between each queued function execution.
+ * @param options  Optional object with configuration properties.
+ */
 export const queue = <T>(callback: (args?: T) => void, delay: number, options: Partial<Options> = defaultOptions) => {
     if (typeof callback !== 'function') {
         throw new TypeError('Expected a function');
