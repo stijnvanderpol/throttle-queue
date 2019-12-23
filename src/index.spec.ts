eba7d6d4a95137098517ec36bc6eeb4dca1b0565
Stijn van der Pol <s.o.vanderpol@gmail.com>
2019-12-23 15:35:06 +0100

    rephrase and reorganize tests to be more uniform and logical

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,19 +2,20 @@ import { stub } from 'sinon';
 import { queue } from './index';
 
 describe('queue', () => {
-    it('invokes the first call right away, by default, and delays subsequent calls', (finish) => {
+    it('executes the first call in the next frame, by default, and delays subsequent calls', (finish) => {
         const callbackStub = stub();
-        const queuedCallbackStub = queue(callbackStub, 200);
+        const queuedCallback = queue(callbackStub, 200);
 
         expect(callbackStub.callCount).toStrictEqual(0);
 
-        queuedCallbackStub(); // called immediately
-        queuedCallbackStub(); // called after 200ms
-        queuedCallbackStub(); // called after 400ms
-    
+        queuedCallback(); // called immediately.
+        queuedCallback(); // called after 200ms.
+        queuedCallback(); // called after 400ms.
+        
+        // delay assertion by 10ms to give JS time to execute the callback in the next frame.
         setTimeout(() => {
             expect(callbackStub.callCount).toStrictEqual(1);
-        }, 100);
+        }, 10);
         
         setTimeout(() => {
             expect(callbackStub.callCount).toStrictEqual(2);
@@ -26,14 +27,14 @@ describe('queue', () => {
         }, 500);
     });
 
-    it('invokes the first call after the delay if skipInitialDelay is disabled', (finish) => {
+    it('executes the first call after the delay if skipInitialDelay is disabled', (finish) => {
         const callbackStub = stub();
-        const queuedCallbackStub = queue(callbackStub, 200, { skipInitialDelay: false });
+        const queuedCallback = queue(callbackStub, 200, { skipInitialDelay: false });
 
         expect(callbackStub.callCount).toStrictEqual(0);
 
-        queuedCallbackStub(); // called after 200ms
-        queuedCallbackStub(); // called after 400ms
+        queuedCallback(); // called after 200ms.
+        queuedCallback(); // called after 400ms.
 
         setTimeout(() => {
             expect(callbackStub.callCount).toStrictEqual(0);
@@ -49,34 +50,20 @@ describe('queue', () => {
         }, 500);
     });
 
-    it('cancels the queue and does not execute the queued callbacks', (finish) => {
-        const callbackStub = stub();
-        const queuedCallbackStub = queue(callbackStub, 200, { skipInitialDelay: false });
-
-        queuedCallbackStub(); // called after 200ms
-        queuedCallbackStub(); // called after 400ms
-        queuedCallbackStub.cancel();
-
-        setTimeout(() => {
-            expect(callbackStub.callCount).toEqual(0);
-            finish();
-        }, 500);
-    });
-    
-    it('calls a subsequent call immediately if the queue has been cleared and the delay has passed', (finish) => {
+    it('executes a subsequent call in the next frame if the queue has been cleared and the delay has passed', (finish) => {
         const callbackStub = stub();
         const queuedCallback = queue(callbackStub, 200);
         
-        queuedCallback(); // called immediately
-        queuedCallback(); // called after 200ms
-        // the queue will be reset after an additional 200ms. 
+        queuedCallback(); // called in the next frame.
+        queuedCallback(); // called after 200ms.
+        // the queue will be reset after an additional 200ms.
 
         // 500ms have passed, the queue should've been reset by now.
         setTimeout(() => {
             expect(callbackStub.callCount).toEqual(2);
             queuedCallback(); // called immediately
             
-            // delay assertion by 10ms to take into account that "immediate" calls are executed 1 frame later.
+            // delay assertion by 10ms to give JS time to execute the callback in the next frame.
             setTimeout(() => {
                 expect(callbackStub.callCount).toEqual(3);
                 finish();
@@ -84,18 +71,32 @@ describe('queue', () => {
         }, 500)
     });
 
+    it('cancels the queue and does not execute the queued callbacks', (finish) => {
+        const callbackStub = stub();
+        const queuedCallback = queue(callbackStub, 200, { skipInitialDelay: false });
+
+        queuedCallback(); // called after 200ms.
+        queuedCallback(); // called after 400ms.
+        queuedCallback.cancel();
+
+        setTimeout(() => {
+            expect(callbackStub.callCount).toEqual(0);
+            finish();
+        }, 500);
+    });
+    
     it('calls a subsequent call in the next frame if the queue has been canceled', (finish) => {
         const callbackStub = stub();
         const queuedCallback = queue(callbackStub, 200);
 
-        queuedCallback(); // called in the next frame
-        queuedCallback(); // called after 200ms
+        queuedCallback(); // called in the next frame.
+        queuedCallback(); // called after 200ms.
         queuedCallback.cancel();
         
         expect(callbackStub.callCount).toEqual(0);
         queuedCallback();
         
-        // delay assertion by 10ms to give JS time to execute the callback in the next frame
+        // delay assertion by 10ms to give JS time to execute the callback in the next frame.
         setTimeout(() => {
             expect(callbackStub.callCount).toEqual(1);
             finish();
