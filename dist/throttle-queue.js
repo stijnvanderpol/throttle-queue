d4f105bc68d56ea3bd769e5a6ca7bdeffaf21035
Stijn van der Pol <s.o.vanderpol@gmail.com>
2019-12-26 17:41:47 +0100

    update the build

--- dist/throttle-queue.js
+++ dist/throttle-queue.js
@@ -1,1 +1,112 @@
-!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define("throttleQueue",[],t):"object"==typeof exports?exports.throttleQueue=t():e.throttleQueue=t()}(window,(function(){return function(e){var t={};function n(o){if(t[o])return t[o].exports;var r=t[o]={i:o,l:!1,exports:{}};return e[o].call(r.exports,r,r.exports,n),r.l=!0,r.exports}return n.m=e,n.c=t,n.d=function(e,t,o){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:o})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(n.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var r in e)n.d(o,r,function(t){return e[t]}.bind(null,r));return o},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var o={skipInitialDelay:!0};t.throttleQueue=function(e,t,n){if(void 0===n&&(n=o),"function"!=typeof e)throw new TypeError("Expected a function");var r,u=[];function i(){for(var o=[],i=0;i<arguments.length;i++)o[i]=arguments[i];u.push({callback:e,args:o});var l=function(e){void 0===e&&(e=t),r=setTimeout((function(){if(u.length){var e=u[0],t=e.callback,n=e.args;u.splice(0,1),t.apply(void 0,n),l()}else r=void 0}),e)};r||l(n.skipInitialDelay?0:void 0)}return i.cancel=function(){u.slice(0,u.length),clearTimeout(r),r=void 0},i},t.default=t.throttleQueue}]).default}));
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define("throttleQueue", [], factory);
+	else if(typeof exports === 'object')
+		exports["throttleQueue"] = factory();
+	else
+		root["throttleQueue"] = factory();
+})(window, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.ts");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/index.ts":
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\nObject.defineProperty(exports, \"__esModule\", { value: true });\nvar defaultOptions = {\n    skipInitialDelay: true\n};\n/**\n * Returns a throttleQueued function that when called will store the calls in a queue\n * and execute them one by one after the provided delay. The first call will be executed\n * in the next frame unless configured otherwise.\n * @param callback The function to transform into a throttleQueued function.\n * @param delay    The time in milliseconds between each queued function execution.\n * @param options  Optional object with configuration properties.\n */\nexports.throttleQueue = function (callback, delay, options) {\n    if (options === void 0) { options = defaultOptions; }\n    if (typeof callback !== 'function') {\n        throw new TypeError('Expected a function');\n    }\n    var callbackQueue = [];\n    var timeout;\n    function cancel() {\n        callbackQueue = [];\n        clearTimeout(timeout);\n        timeout = undefined;\n    }\n    function queuedCallback() {\n        var args = [];\n        for (var _i = 0; _i < arguments.length; _i++) {\n            args[_i] = arguments[_i];\n        }\n        callbackQueue.push({ callback: callback, args: args });\n        var run = function (recursiveDelay) {\n            if (recursiveDelay === void 0) { recursiveDelay = delay; }\n            timeout = setTimeout(function () {\n                if (callbackQueue.length) {\n                    var _a = callbackQueue[0], queuedCall = _a.callback, queuedArgs = _a.args;\n                    callbackQueue.splice(0, 1);\n                    queuedCall.apply(void 0, queuedArgs);\n                    run();\n                }\n                else {\n                    timeout = undefined;\n                }\n            }, recursiveDelay);\n        };\n        if (!timeout) {\n            run(options.skipInitialDelay ? 0 : undefined);\n        }\n    }\n    queuedCallback.cancel = cancel;\n    return queuedCallback;\n};\nexports.default = exports.throttleQueue;\n\n\n//# sourceURL=webpack://throttleQueue/./src/index.ts?");
+
+/***/ })
+
+/******/ })["default"];
+});
